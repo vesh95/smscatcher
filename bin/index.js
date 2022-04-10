51bb339f251a6d301bbae77b45ffdedbcc065e71
Eduard Larionov <vesh95.17@ya.ru>
2022-04-10 18:56:47 +0500

    Move files

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const apiRoutes = require('./src/api');
-const pageRoute = require('./src/pages');
+const apiRoutes = require('../src/api');
+const pageRoute = require('../src/pages');
 
 app.set('title', 'SMSCatcher')
 app.set('view engine', 'ejs')
-
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, '../src/views'))
+app.use(express.static(path.join(__dirname, '/../public')))
 app.use(express.json())
 app.use(apiRoutes)
 app.use(pageRoute)
